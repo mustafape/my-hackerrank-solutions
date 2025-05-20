e5eb9f540c52ee3788174496ae4c25d5cff9aab4
Mustafa Peker <mustafa.kamer.peker@gmail.com>
2025-05-20 09:48:23 +0200

    Update CPPClassTemplateSpecialization.cpp
    Added solution part separately.

--- OtherConcepts/CPPClassTemplateSpecialization.cpp
+++ OtherConcepts/CPPClassTemplateSpecialization.cpp
@@ -6,7 +6,27 @@ enum class Color { red, green, orange };
 template <typename T> struct Traits;
 
 // Define specializations for the Traits class template here.
+template <> struct Traits<Fruit> {
+    static const char* name(int index) {
+        switch (index) {
+            case 0: return "apple";
+            case 1: return "orange";
+            case 2: return "pear";
+            default: return "unknown";
+        }
+    }
+};
 
+template <> struct Traits<Color> {
+    static const char* name(int index) {
+        switch (index) {
+            case 0: return "red";
+            case 1: return "green";
+            case 2: return "orange";
+            default: return "unknown";
+        }
+    }
+};
 
 int main()
 {
